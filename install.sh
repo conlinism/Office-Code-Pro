023ec11479ad9ff2eb4fc7c0fa758d463bbcee8d
Conlin Durbin <conlindurbin@gmail.com>
2015-04-19 22:52:52 -0400

    Added Install file

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Do we have git?
+if test ! $(which git); then
+  echo "Must have git installed for this to work!"
+  exit 0
+fi
+
+#Download the repo and cd to fonts folder (Thanks to w0ng!)
+git clone https://github.com/nathco/Office-Code-Pro.git Office-Code-Pro
+cd Office-Code-Pro/Fonts/OTF
+
+#Magical find command to cp all fonts to /Library/Fonts
+find . -name "*.otf" -exec cp {} /Library/Fonts/ \;
+
+#All done, let's clean up a bit
+cd ../..
+rm -rf Office-Code-Pro
